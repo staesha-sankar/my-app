50d153a7673736b7986e80a7dcb352ae469f48d0
staesha-sankar <staesha.sankar@sanlam.co.za>
2026-07-02 13:31:27 +0200

    react

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import { MovieCounter } from "./MovieCounter";
 
 //presentation
-
 
 // Task 2.1
 // If rating is 8 or more (green) otherwise (red)
 //presentation
 export function Movie({ name, poster, summary, rating }) {
-
+  const [showSummary, setShowSummary] = useState(true);
   const style = {
     color: rating >= 8 ? "green" : "red",
+  };
+
+  const summaryStyle = {
+    display: showSummary ? "block" : "none",
   }
+
   return (
     <section className="movie-container">
       <img src={poster} alt="" />
@@ -21,8 +26,21 @@ export function Movie({ name, poster, summary, rating }) {
             ⭐ {rating}
           </p>
         </div>
-        <button>💡 Toggle Summary</button>
-        <p className="movie-summary">{summary}</p>
+        <button onClick={() => setShowSummary(!showSummary)}>
+          💡 Toggle Summary
+        </button>
+        {/* Conditional styling - remains in DOM*/}
+        <p
+          className="movie-summary"
+          style={summaryStyle}
+        >
+          {summary}
+        </p>
+
+        {/* Conditional rendering - removes from DOM*/}
+
+        {/* showSummary ? <p className="movie-summary">{summary}</p> : null */}
+
         <MovieCounter />
       </div>
     </section>

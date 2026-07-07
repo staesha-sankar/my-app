a679e9bc72e598cf6d0f18b513148e402b28c464
staesha-sankar <staesha.sankar@sanlam.co.za>
2026-07-07 08:58:00 +0200

    material ui

--- src/Movies/Movie.jsx
+++ src/Movies/Movie.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { MovieCounter } from "./MovieCounter";
 
-//presentation
+// // //presentation
 
-// Task 2.1
-// If rating is 8 or more (green) otherwise (red)
-//presentation
+// // // Task 2.1
+// // // If rating is 8 or more (green) otherwise (red)
+// // //presentation
 export function Movie({ name, poster, summary, rating }) {
   const [showSummary, setShowSummary] = useState(true);
   const style = {
@@ -46,3 +46,82 @@ export function Movie({ name, poster, summary, rating }) {
     </section>
   );
 }
+
+
+// import { useState } from "react";
+// import { MovieCounter } from "./MovieCounter";
+
+// import Card from "@mui/material/Card";
+// import CardMedia from "@mui/material/CardMedia";
+// import CardContent from "@mui/material/CardContent";
+// import Typography from "@mui/material/Typography";
+// import IconButton from "@mui/material/IconButton";
+
+// //presentation
+
+// // Task 2.1
+// // If rating is 8 or more (green) otherwise (red)
+// //presentation
+// export function Movie({ name, poster, summary, rating }) {
+//   const [showSummary, setShowSummary] = useState(true);
+
+//   const style = {
+//     color: rating >= 8 ? "green" : "red",
+//   };
+
+//   const summaryStyle = {
+//     display: showSummary ? "block" : "none",
+//   };
+
+//   return (
+//     <Card className="movie-container">
+//       <CardMedia
+//         component="img"
+//         image={poster}
+//         alt={name}
+//         className="movie-poster"
+//       />
+
+//       <CardContent className="movie-details">
+//         <div className="movie-spec-container">
+//           <Typography variant="h5" component="h2" className="movie-name">
+//             {name}
+//           </Typography>
+
+//           <Typography
+//             variant="h6"
+//             component="p"
+//             className="movie-rating"
+//             style={style}
+//           >
+//             ⭐ {rating}
+//           </Typography>
+//         </div>
+
+//         <IconButton
+//           onClick={() => setShowSummary(!showSummary)}
+//           className="toggle-summary-button"
+//           aria-label="toggle summary"
+//         >
+//           {showSummary ? "⬆️" : "⬇️"}
+//         </IconButton>
+
+//         {/* Conditional styling - remains in DOM*/}
+//         <Typography
+//           variant="body2"
+//           component="p"
+//           className="movie-summary"
+//           style={summaryStyle}
+//         >
+//           {summary}
+//         </Typography>
+
+//         {/* Conditional rendering - removes from DOM*/}
+
+//         {/* showSummary ? <p className="movie-summary">{summary}</p> : null */}
+
+//         <MovieCounter />
+//       </CardContent>
+//     </Card>
+//   );
+// }
--- src/Movies/MovieCounter.jsx
+++ src/Movies/MovieCounter.jsx
@@ -24,3 +24,41 @@ export function MovieCounter() {
     </div>
   );
 }
+
+// import { useState } from "react";
+
+// import IconButton from "@mui/material/IconButton";
+// import Badge from "@mui/material/Badge";
+
+// export function MovieCounter() {
+//   let [like, setLike] = useState(0);
+//   let [disLike, setDisLike] = useState(0);
+
+//   return (
+//     <div>
+//       <div className="action-buttons">
+//         <Badge badgeContent={like} color="primary">
+//           <IconButton
+//             onClick={() => {
+//               setLike(like + 1);
+//             }}
+//             aria-label="like movie"
+//           >
+//             👍
+//           </IconButton>
+//         </Badge>
+
+//         <Badge badgeContent={disLike} color="error">
+//           <IconButton
+//             onClick={() => {
+//               setDisLike(disLike + 1);
+//             }}
+//             aria-label="dislike movie"
+//           >
+//             👎
+//           </IconButton>
+//         </Badge>
+//       </div>
+//     </div>
+//   );
+// }
